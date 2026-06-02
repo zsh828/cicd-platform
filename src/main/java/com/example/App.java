112cc71b68d9ff136b9e9553e78e57ddd1f16a48
zsh828 <1657477664@qq.com>
2026-06-02 14:35:56 +0800

    fix: 自动修复 Build #9999 编译错误

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello from CICD Platform!");
+        System.out.println("Hello, world!");
     }
 }
